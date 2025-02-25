9680980b67726d878311ee44ed76b19c13209f0c
HectorAyusoMartin <hector.ayuso.martin@gmail.com>
2025-02-25 11:11:36 +0100

    Corregido acceso a resultados de JSON

--- nmap_scanner.py
+++ nmap_scanner.py
@@ -6,13 +6,20 @@ def hosts_scan(network):
     Realiza un escaneo de hosts usando nmap y nos devuelve un JSON con todos los datos
     """
     
-    nm = nmap.PortScanner()
+    #nm = nmap.PortScanner()
+    nm = nmap.PortScanner(nmap_search_path=('C:/Program Files (x86)/Nmap/nmap.exe',))
+
     nm.scan(hosts=network, arguments='-sn')
+    #print('Comando nmap: ', nm.command_line())
+    
+    #Salida completa de nmap
+    #for host in nm.all_hosts():
+        #print(f'{host}: {nm[host]}')
     
     #Comprobamos que los hosts que nos devuelve están todos activos
-    active_hosts = [host for host in nm.all_hosts() if nm[host].state == 'up']
+    active_hosts = [host for host in nm.all_hosts() if nm[host]['status']['state'] == 'up']
     #Explicación del comprehension list de arriba: Para cada host dentro de nm.all_host, en el caso de que para ese host su estado sea 'up'(activo), entonces concatenalo a la lista active_hosts
-    return active_hosts
+    return print('Host activos en la Red'),active_hosts
     
     
     
